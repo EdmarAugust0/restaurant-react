86528be00eb54ccc0f066e4254cdb032c2111ed3
EdmarAugust0 <edmar.contato149@gmail.com>
2024-08-14 17:01:06 -0300

    :construction:

--- src/components/FormAddress/index.tsx
+++ src/components/FormAddress/index.tsx
@@ -13,17 +13,22 @@ type Props = {
 }
 
 const Address = ({ setNext }: Props) => {
-  const [toPayment, setToPayment] = useState(true)
+  const [toPayment, setToPayment] = useState(false)
   const [purchase, { isError, isLoading, data }] = usePurchaseMutation()
 
-  const formAddress = useFormik({
+  const form = useFormik({
     initialValues: {
       name: '',
       address: '',
       city: '',
       cep: '',
       numberHouse: '',
-      complement: ''
+      complement: '',
+      nameCard: '',
+      numberCard: '',
+      cvv: '',
+      expiresMonth: '',
+      expiresYear: ''
     },
     validationSchema: Yup.object({
       name: Yup.string()
@@ -34,7 +39,15 @@ const Address = ({ setNext }: Props) => {
       cep: Yup.string()
         .min(9, 'Favor informar todos os numeros contando com o traço')
         .required('Campo obrigatório'),
-      numberHouse: Yup.string().required('Campo obrigatório')
+      numberHouse: Yup.string().required('Campo obrigatório'),
+
+      nameCard: Yup.string()
+        .min(5, 'O nome deve ter no mínimo 5 caracteres')
+        .required('Campo obrigatório'),
+      numberCard: Yup.string().required('Campo obrigatório'),
+      cvv: Yup.string().required('Campo obrigatório'),
+      expiresMonth: Yup.string().required('Campo obrigatório'),
+      expiresYear: Yup.string().required('Campo obrigatório')
     }),
     onSubmit: (values) => {
       purchase({
@@ -47,30 +60,7 @@ const Address = ({ setNext }: Props) => {
             number: values.numberHouse,
             complement: values.complement
           }
-        }
-      })
-    }
-  })
-
-  const formPayment = useFormik({
-    initialValues: {
-      nameCard: '',
-      numberCard: '',
-      cvv: '',
-      expiresMonth: '',
-      expiresYear: ''
-    },
-    validationSchema: Yup.object({
-      nameCard: Yup.string()
-        .min(5, 'O nome deve ter no mínimo 5 caracteres')
-        .required('Campo obrigatório'),
-      numberCard: Yup.string().required('Campo obrigatório'),
-      cvv: Yup.string().required('Campo obrigatório'),
-      expiresMonth: Yup.string().required('Campo obrigatório'),
-      expiresYear: Yup.string().required('Campo obrigatório')
-    }),
-    onSubmit: (values) => {
-      purchase({
+        },
         payment: {
           card: {
             name: values.nameCard,
@@ -81,22 +71,20 @@ const Address = ({ setNext }: Props) => {
               year: values.expiresYear
             }
           }
-        }
+        },
+        products: [
+          {
+            id: 1,
+            price: 10
+          }
+        ]
       })
     }
   })
 
-  const getErrorMessageAddress = (fieldName: string, message?: string) => {
-    const isTouched = fieldName in formAddress.touched
-    const isInvalid = fieldName in formAddress.errors
-
-    if (isTouched && isInvalid) return message
-    return ''
-  }
-
-  const getErrorMessagePayment = (fieldName: string, message?: string) => {
-    const isTouched = fieldName in formPayment.touched
-    const isInvalid = fieldName in formPayment.errors
+  const getErrorMessage = (fieldName: string, message?: string) => {
+    const isTouched = fieldName in form.touched
+    const isInvalid = fieldName in form.errors
 
     if (isTouched && isInvalid) return message
     return ''
@@ -104,223 +92,196 @@ const Address = ({ setNext }: Props) => {
 
   if (toPayment) {
     return (
-      <form onSubmit={formPayment.handleSubmit}>
-        <FormAddress>
-          <h1>Pagamento - Valor a pagar R$ 200,00 </h1>
-          <div>
-            <InputGroup>
-              <label htmlFor="nameCard">Nome no cartão</label>
-              <input
-                type="text"
-                id="nameCard"
-                name="nameCard"
-                value={formPayment.values.nameCard}
-                onChange={formPayment.handleChange}
-                onBlur={formPayment.handleBlur}
-              />
-              <small>
-                {getErrorMessagePayment(
-                  'nameCard',
-                  formPayment.errors.nameCard
-                )}
-              </small>
-            </InputGroup>
-            <MiniInputGroup>
-              <InputGroup>
-                <label htmlFor="numberCard">Número do cartão</label>
-                <input
-                  type="text"
-                  id="numberCard"
-                  name="numberCard"
-                  value={formPayment.values.numberCard}
-                  onChange={formPayment.handleChange}
-                  onBlur={formPayment.handleBlur}
-                />
-                <small>
-                  {getErrorMessagePayment(
-                    'numberCard',
-                    formPayment.errors.numberCard
-                  )}
-                </small>
-              </InputGroup>
-              <InputGroup maxWidth="88px">
-                <label htmlFor="cvv">CVV</label>
-                <input
-                  type="text"
-                  id="cvv"
-                  name="cvv"
-                  value={formPayment.values.cvv}
-                  onChange={formPayment.handleChange}
-                  onBlur={formPayment.handleBlur}
-                />
-                <small>
-                  {getErrorMessagePayment('cvv', formPayment.errors.cvv)}
-                </small>
-              </InputGroup>
-            </MiniInputGroup>
-            <MiniInputGroup>
-              <InputGroup>
-                <label htmlFor="expiresMonth">Mês de vencimento</label>
-                <input
-                  type="text"
-                  id="expiresMonth"
-                  name="expiresMonth"
-                  value={formPayment.values.expiresMonth}
-                  onChange={formPayment.handleChange}
-                  onBlur={formPayment.handleBlur}
-                />
-                <small>
-                  {getErrorMessagePayment(
-                    'expiresMonth',
-                    formPayment.errors.expiresMonth
-                  )}
-                </small>
-              </InputGroup>
-              <InputGroup>
-                <label htmlFor="expiresYear">Ano de vencimento</label>
-                <input
-                  type="text"
-                  id="expiresYear"
-                  name="expiresYear"
-                  value={formPayment.values.expiresYear}
-                  onChange={formPayment.handleChange}
-                  onBlur={formPayment.handleBlur}
-                />
-                <small>
-                  {getErrorMessagePayment(
-                    'expiresYear',
-                    formPayment.errors.expiresYear
-                  )}
-                </small>
-              </InputGroup>
-            </MiniInputGroup>
-          </div>
-          <ButtonGroup>
-            <Button
-              type="submit"
-              title="Finalizar pagamento"
-              onClick={formPayment.handleSubmit}
-            >
-              Finalizar pagamento
-            </Button>
-            <Button type="button" title="Voltar para a edição de endereço">
-              Voltar para a edição de endereço
-            </Button>
-          </ButtonGroup>
-        </FormAddress>
-      </form>
-    )
-  }
-  return (
-    <form onSubmit={formAddress.handleSubmit}>
-      <FormAddress>
-        <h1>Entrega</h1>
+      <FormAddress onSubmit={form.handleSubmit}>
+        <h1>Pagamento - Valor a pagar R$ 200,00 </h1>
         <div>
           <InputGroup>
-            <label htmlFor="name">Quem irá receber</label>
+            <label htmlFor="nameCard">Nome no cartão</label>
             <input
               type="text"
-              id="name"
-              name="name"
-              value={formAddress.values.name}
-              onChange={formAddress.handleChange}
-              onBlur={formAddress.handleBlur}
+              id="nameCard"
+              name="nameCard"
+              value={form.values.nameCard}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
             />
-            <small>
-              {getErrorMessageAddress('name', formAddress.errors.name)}
-            </small>
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor="address">Endereço</label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              value={formAddress.values.address}
-              onChange={formAddress.handleChange}
-              onBlur={formAddress.handleBlur}
-            />
-            <small>
-              {getErrorMessageAddress('address', formAddress.errors.address)}
-            </small>
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor="city">Cidade</label>
-            <input
-              type="text"
-              id="city"
-              name="city"
-              value={formAddress.values.city}
-              onChange={formAddress.handleChange}
-              onBlur={formAddress.handleBlur}
-            />
-            <small>
-              {getErrorMessageAddress('city', formAddress.errors.city)}
-            </small>
+            <small>{getErrorMessage('nameCard', form.errors.nameCard)}</small>
           </InputGroup>
           <MiniInputGroup>
             <InputGroup>
-              <label htmlFor="cep">CEP</label>
+              <label htmlFor="numberCard">Número do cartão</label>
               <input
                 type="text"
-                id="cep"
-                name="cep"
-                value={formAddress.values.cep}
-                onChange={formAddress.handleChange}
-                onBlur={formAddress.handleBlur}
+                id="numberCard"
+                name="numberCard"
+                value={form.values.numberCard}
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
               />
               <small>
-                {getErrorMessageAddress('cep', formAddress.errors.cep)}
+                {getErrorMessage('numberCard', form.errors.numberCard)}
+              </small>
+            </InputGroup>
+            <InputGroup maxWidth="88px">
+              <label htmlFor="cvv">CVV</label>
+              <input
+                type="text"
+                id="cvv"
+                name="cvv"
+                value={form.values.cvv}
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+              />
+              <small>{getErrorMessage('cvv', form.errors.cvv)}</small>
+            </InputGroup>
+          </MiniInputGroup>
+          <MiniInputGroup>
+            <InputGroup>
+              <label htmlFor="expiresMonth">Mês de vencimento</label>
+              <input
+                type="text"
+                id="expiresMonth"
+                name="expiresMonth"
+                value={form.values.expiresMonth}
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+              />
+              <small>
+                {getErrorMessage('expiresMonth', form.errors.expiresMonth)}
               </small>
             </InputGroup>
             <InputGroup>
-              <label htmlFor="numberHouse">Número</label>
+              <label htmlFor="expiresYear">Ano de vencimento</label>
               <input
                 type="text"
-                id="numberHouse"
-                name="numberHouse"
-                value={formAddress.values.numberHouse}
-                onChange={formAddress.handleChange}
-                onBlur={formAddress.handleBlur}
+                id="expiresYear"
+                name="expiresYear"
+                value={form.values.expiresYear}
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
               />
               <small>
-                {getErrorMessageAddress(
-                  'numberHouse',
-                  formAddress.errors.numberHouse
-                )}
+                {getErrorMessage('expiresYear', form.errors.expiresYear)}
               </small>
             </InputGroup>
           </MiniInputGroup>
-          <InputGroup>
-            <label htmlFor="complement">Complemento (opcional)</label>
-            <input
-              type="text"
-              id="complement"
-              name="complement"
-              value={formAddress.values.complement}
-              onChange={formAddress.handleChange}
-              onBlur={formAddress.handleBlur}
-            />
-          </InputGroup>
         </div>
         <ButtonGroup>
           <Button
             type="submit"
-            title="Continuar com o pagamento"
-            onClick={formAddress.handleSubmit}
+            title="Finalizar pagamento"
+            onClick={form.handleSubmit}
           >
-            Continuar com o pagamento
+            Finalizar pagamento
           </Button>
           <Button
             type="button"
-            title="Voltar para o carrinho"
-            onClick={() => setNext(false)}
+            title="Voltar para a edição de endereço"
+            onClick={() => setToPayment(false)}
           >
-            Voltar para o carrinho
+            Voltar para a edição de endereço
           </Button>
         </ButtonGroup>
       </FormAddress>
-    </form>
+    )
+  }
+  return (
+    <FormAddress onSubmit={form.handleSubmit}>
+      <h1>Entrega</h1>
+      <div>
+        <InputGroup>
+          <label htmlFor="name">Quem irá receber</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={form.values.name}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+          />
+          <small>{getErrorMessage('name', form.errors.name)}</small>
+        </InputGroup>
+        <InputGroup>
+          <label htmlFor="address">Endereço</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={form.values.address}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+          />
+          <small>{getErrorMessage('address', form.errors.address)}</small>
+        </InputGroup>
+        <InputGroup>
+          <label htmlFor="city">Cidade</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            value={form.values.city}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+          />
+          <small>{getErrorMessage('city', form.errors.city)}</small>
+        </InputGroup>
+        <MiniInputGroup>
+          <InputGroup>
+            <label htmlFor="cep">CEP</label>
+            <input
+              type="text"
+              id="cep"
+              name="cep"
+              value={form.values.cep}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>{getErrorMessage('cep', form.errors.cep)}</small>
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor="numberHouse">Número</label>
+            <input
+              type="text"
+              id="numberHouse"
+              name="numberHouse"
+              value={form.values.numberHouse}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>
+              {getErrorMessage('numberHouse', form.errors.numberHouse)}
+            </small>
+          </InputGroup>
+        </MiniInputGroup>
+        <InputGroup>
+          <label htmlFor="complement">Complemento (opcional)</label>
+          <input
+            type="text"
+            id="complement"
+            name="complement"
+            value={form.values.complement}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+          />
+        </InputGroup>
+      </div>
+      <ButtonGroup>
+        <Button
+          type="submit"
+          title="Continuar com o pagamento"
+          onClick={() => setToPayment(true)}
+        >
+          Continuar com o pagamento
+        </Button>
+        <Button
+          type="button"
+          title="Voltar para o carrinho"
+          onClick={() => setNext(false)}
+        >
+          Voltar para o carrinho
+        </Button>
+      </ButtonGroup>
+    </FormAddress>
   )
 }
 
